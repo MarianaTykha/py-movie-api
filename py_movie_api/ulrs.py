6eaccb5ee1e7452c5c1a82885904a7ed42eaf4fc
Мар'яна Тиха <mariana@tikhaya@gmail.com>
2025-03-08 21:13:55 +0200

    Update .gitignore, urls, and remove old files

--- py_movie_api/ulrs.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from cinema import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/cinema-halls/', views.cinema_halls_list),
-    path('', views.home),
-]
